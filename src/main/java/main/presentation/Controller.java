523ab9629f198c1cb22fc9f55466626187fc429b
aottolini <aottolini@agro.uba.ar>
2021-03-20 17:14:38 -0300

    added test hello

--- src/main/java/main/presentation/Controller.java
+++ src/main/java/main/presentation/Controller.java
@@ -2,6 +2,7 @@ package main.presentation;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class Controller {
 	Controller(ItemDataHandler consumer) {
 		super();
 		this.fetcher = consumer;
+	}
+
+	@GetMapping("hello")
+	String getItem() {
+		return "hello";
 	}
 
 	@PostMapping("quote")
